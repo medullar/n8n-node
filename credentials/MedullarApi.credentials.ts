fb1e5b021ab2ea96fb21f2a21ee90c5ef555e6fe
Marc Llopart <marc@medullar.com>
2025-06-13 10:59:28 +0200

    chore: update package.json for Medullar node package
    - Changed package name to "n8n-nodes-medullar"
    - Updated description to "n8n Community Node Package for Medullar"
    - Set author details to "Medullar Solutions Inc." with email "hello@medullar.com"
    - Updated repository URL to "https://github.com/medullar/n8n-node.git"
    - Modified credentials and nodes paths to reflect Medullar-specific files

---
+++ credentials/MedullarApi.credentials.ts
@@ -0,0 +1,39 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class MedullarApi implements ICredentialType {
+	name = 'medullarApi';
+	displayName = 'Medullar API';
+	documentationUrl = 'https://documentation.medullar.com/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			default: '',
+			typeOptions: {
+				password: true,
+			},
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '=Bearer {{$credentials.apiKey}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.medullar.com/auth/v1',
+			url: '/users/me/',
+		},
+	};
+}
---
+++ nodes/Medullar/Medullar.node.ts
@@ -0,0 +1,160 @@
+import type {
+	IDataObject,
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	IRequestOptions,
+} from 'n8n-workflow';
+import { NodeConnectionType } from 'n8n-workflow';
+
+export class Medullar implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Medullar',
+		icon: 'file:medullar_icon.svg',
+		name: 'medullar',
+		group: ['transform'],
+		version: 1,
+		description: 'Basic Medullar Node',
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		defaults: {
+			name: 'Medullar',
+		},
+		inputs: [NodeConnectionType.Main],
+		outputs: [NodeConnectionType.Main],
+		usableAsTool: true,
+		credentials: [
+			{
+				name: 'medullarApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				options: [
+					{
+						name: 'Contact',
+						value: 'contact',
+					},
+				],
+				default: 'contact',
+				noDataExpression: true,
+				required: true,
+				description: 'Create a new contact',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				displayOptions: {
+					show: {
+						resource: ['contact'],
+					},
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						description: 'Create a contact',
+						action: 'Create a contact',
+					},
+				],
+				default: 'create',
+				noDataExpression: true,
+			},
+			{
+				displayName: 'Email',
+				name: 'email',
+				type: 'string',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['create'],
+						resource: ['contact'],
+					},
+				},
+				default: '',
+				placeholder: 'name@email.com',
+				description: 'Primary email for the contact',
+			},
+
+			{
+				displayName: 'Additional Fields',
+				name: 'additionalFields',
+				type: 'collection',
+				placeholder: 'Add Field',
+				default: {},
+				displayOptions: {
+					show: {
+						resource: ['contact'],
+						operation: ['create'],
+					},
+				},
+				options: [
+					{
+						displayName: 'First Name',
+						name: 'firstName',
+						type: 'string',
+						default: '',
+					},
+					{
+						displayName: 'Last Name',
+						name: 'lastName',
+						type: 'string',
+						default: '',
+					},
+				],
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		// Handle data coming from previous nodes
+		const items = this.getInputData();
+		let responseData;
+		const returnData = [];
+		const resource = this.getNodeParameter('resource', 0) as string;
+		const operation = this.getNodeParameter('operation', 0) as string;
+
+		// For each item, make an API call to create a contact
+		for (let i = 0; i < items.length; i++) {
+			if (resource === 'contact') {
+				if (operation === 'create') {
+					// Get email input
+					const email = this.getNodeParameter('email', i) as string;
+					// Get additional fields input
+					const additionalFields = this.getNodeParameter('additionalFields', i) as IDataObject;
+					const data: IDataObject = {
+						email,
+					};
+
+					Object.assign(data, additionalFields);
+
+					// Make HTTP request according to https://sendgrid.com/docs/api-reference/
+					const options: IRequestOptions = {
+						headers: {
+							Accept: 'application/json',
+						},
+						method: 'PUT',
+						body: {
+							contacts: [data],
+						},
+						uri: `https://api.sendgrid.com/v3/marketing/contacts`,
+						json: true,
+					};
+					responseData = await this.helpers.requestWithAuthentication.call(
+						this,
+						'friendGridApi',
+						options,
+					);
+					returnData.push(responseData);
+				}
+			}
+		}
+		// Map data to n8n data structure
+		return [this.helpers.returnJsonArray(returnData)];
+	}
+}
